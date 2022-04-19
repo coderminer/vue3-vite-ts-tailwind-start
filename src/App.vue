613bdefd027156e7ba19a7de7694e67bfde00380
kevin <kevin@puhanda.com>
2022-04-19 08:23:30 +0800

    feat: base version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div class="h-screenbg-red-300"></div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
